e43b5f103130c4fc1172131567bea2a32648511c
lcamilo <lhenriquegomescamilo@gmail.com>
2019-04-08 23:12:37 -0300

    configurando microservie de database para receber a variável de ambiente
    - microservice.address.ts: Recebendo pela variável de ambiente a url do servidor
    - database.providers.ts: Corrigindo indentação
    - 00-mysql-deploy.yaml: Diminuindo a quantidade de  réplicadas
    - 02-user-service.yaml: Criando servidor para o microservice de user

--- database/src/database/database.providers.ts
+++ database/src/database/database.providers.ts
@@ -1,22 +1,23 @@
-import { Sequelize } from 'sequelize-typescript';
-import { User } from '../users/user.entity';
+import {Sequelize} from 'sequelize-typescript';
+import {User} from '../users/user.entity';
 
 export const DatabaseProviders = [
-  {
-    provide: 'SequelizeToken',
-    useFactory: async () => {
-      const sequelize = new Sequelize({
-        operatorsAliases: false,
-        dialect: 'mysql',
-        host: 'mysql',
-        port: 3306,
-        username: 'root',
-        password: 'ENGLAND_IS_MY_CITY',
-        database: 'microservices',
-      });
-      sequelize.addModels([User]);
-      await sequelize.sync();
-      return sequelize;
+    {
+        provide: 'SequelizeToken',
+        useFactory: async () => {
+            const sequelize = new Sequelize({
+                operatorsAliases: false,
+                dialect: 'mysql',
+                host: 'mysql',
+                port: 3306,
+                username: 'root',
+                password: 'ENGLAND_IS_MY_CITY',
+                database: 'microservices',
+                url: process.env.MYSQL_URL
+,            });
+            sequelize.addModels([User]);
+            await sequelize.sync();
+            return sequelize;
+        },
     },
-  },
 ];
