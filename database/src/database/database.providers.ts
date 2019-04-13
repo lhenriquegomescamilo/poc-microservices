c95e2863c72fdb1ed2c770e2f6f5e4c61b1777b4
lcamilo <lhenriquegomescamilo@gmail.com>
2019-04-13 20:00:17 -0300

    Corrigindo url de conexão com o banco

--- database/src/database/database.providers.ts
+++ database/src/database/database.providers.ts
@@ -2,7 +2,7 @@ import {Sequelize} from 'sequelize-typescript';
 import {User} from '../users/user.entity';
 
 const port = (process.env.MYSQL_PORT || 3306) as number;
-const url = (process.env.MYSQL_URL || 'localhost') as string;
+const host = (process.env.MYSQL_URL || 'localhost') as string;
 export const DatabaseProviders = [
     {
         provide: 'SequelizeToken',
@@ -10,12 +10,11 @@ export const DatabaseProviders = [
             const sequelize = new Sequelize({
                 operatorsAliases: false,
                 dialect: 'mysql',
-                host: 'mysql',
+                host,
                 port,
                 username: 'root',
                 password: 'ENGLAND_IS_MY_CITY',
                 database: 'microservices',
-                url,
             });
             sequelize.addModels([User]);
             await sequelize.sync();
