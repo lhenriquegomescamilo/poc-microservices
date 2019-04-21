b4eba1af76ef6e0b7763f08db3c5731bf9bf3ff7
lcamilo <lhenriquegomescamilo@gmail.com>
2019-04-21 17:11:02 -0300

    Extraindo para variáveis comuns

--- devops/pipelines-ci-cd-jenkins/pipelines/database-pipeline.groovy
+++ devops/pipelines-ci-cd-jenkins/pipelines/database-pipeline.groovy
@@ -9,11 +9,18 @@ podTemplate(
         ],
         volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
     node('poc-microservices') {
-        def repos
+        def REPOSITORY
+        def DOCKER_IMAGE = "gateway_database"
+        def DOCKER_IMAGE_VERSION = "gateway_database"
+        def MICROSERVICE_NAME = "database"
+
+        def REPO_HELM_NAME = "poc-microservice"
+        def HELM_SERVICE_URL = "http://helm-chartmuseum:8080"
+
         stage('Checkout') {
             echo 'Iniciando clone do Repositorio'
-            repos = git credentialsId: 'github', url: 'git@github.com:lhenriquegomescamilo/poc-microservices.git'
-            echo repos.toString()
+            REPOSITORY = git credentialsId: 'github', url: 'git@github.com:lhenriquegomescamilo/poc-microservices.git'
+            echo REPOSITORY.toString()
             sh "ls -ltra ./database"
         }
 
@@ -23,8 +30,8 @@ podTemplate(
                 withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'DOCKER_HUB_PASSWORD', usernameVariable: 'DOCKER_HUB_USER')]) {
                     echo 'Building com npm repositorio'
                     sh "docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}"
-                    sh "docker build -t lcamilo/gateway_database:0.0.3 ./database"
-                    sh "docker push lcamilo/gateway_database:0.0.3 "
+                    sh "docker build -t ${DOCKER_HUB_USER}/${DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} ./${MICROSERVICE_NAME}"
+                    sh "docker push ${DOCKER_HUB_USER}/${DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION}"
                 }
             }
         }
@@ -32,13 +39,12 @@ podTemplate(
         stage('Deploy') {
             container('helm-container') {
                 echo 'Deploy com helm'
-                sh "ls -ltra ./database/"
                 sh "helm init --client-only"
-                sh "helm repo add poc-microservice http://helm-chartmuseum:8080"
+                sh "helm repo add ${REPO_HELM_NAME} ${HELM_SERVICE_URL}"
                 sh "helm repo update"
                 sh "helm repo list"
-                sh "helm search poc-microservice"
-                sh "helm upgrade database poc-microservice/database --set image.tag=0.0.3"
+                sh "helm search ${REPO_HELM_NAME}"
+                sh "helm upgrade ${MICROSERVICE_NAME} ${REPO_HELM_NAME}/${MICROSERVICE_NAME} --set image.tag=${DOCKER_IMAGE_VERSION}"
 
             }
         }
